1cd75b757004be1996fc91640162d3b2d6d9f282
Andrzej Simiński <andrzej.simin@gmail.com>
2023-02-03 17:26:12 +0100

    final

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
-docker build -t AndrzejSimin/multi-client-k8s:latest -t AndrzejSimin/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
-docker build -t AndrzejSimin/multi-server-k8s-pgfix:latest -t AndrzejSimin/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
-docker build -t AndrzejSimin/multi-worker-k8s:latest -t AndrzejSimin/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
+docker build -t andrzejsimin/multi-client-k8s:latest -t andrzejsimin/multi-client-k8s:$SHA -f ./client/Dockerfile ./client
+docker build -t andrzejsimin/multi-server-k8s-pgfix:latest -t andrzejsimin/multi-server-k8s-pgfix:$SHA -f ./server/Dockerfile ./server
+docker build -t andrzejsimin/multi-worker-k8s:latest -t andrzejsimin/multi-worker-k8s:$SHA -f ./worker/Dockerfile ./worker
 
-docker push AndrzejSimin/multi-client-k8s:latest
-docker push AndrzejSimin/multi-server-k8s-pgfix:latest
-docker push AndrzejSimin/multi-worker-k8s:latest
+docker push andrzejsimin/multi-client-k8s:latest
+docker push andrzejsimin/multi-server-k8s-pgfix:latest
+docker push andrzejsimin/multi-worker-k8s:latest
 
-docker push AndrzejSimin/multi-client-k8s:$SHA
-docker push AndrzejSimin/multi-server-k8s-pgfix:$SHA
-docker push AndrzejSimin/multi-worker-k8s:$SHA
+docker push andrzejsimin/multi-client-k8s:$SHA
+docker push andrzejsimin/multi-server-k8s-pgfix:$SHA
+docker push andrzejsimin/multi-worker-k8s:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=AndrzejSimin/multi-server-k8s-pgfix:$SHA
-kubectl set image deployments/client-deployment client=AndrzejSimin/multi-client-k8s:$SHA
-kubectl set image deployments/worker-deployment worker=AndrzejSimin/multi-worker-k8s:$SHA
+kubectl set image deployments/server-deployment server=andrzejsimin/multi-server-k8s-pgfix:$SHA
+kubectl set image deployments/client-deployment client=andrzejsimin/multi-client-k8s:$SHA
+kubectl set image deployments/worker-deployment worker=andrzejsimin/multi-worker-k8s:$SHA
